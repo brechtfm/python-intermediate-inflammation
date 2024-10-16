67845ecd5d97381a400b37c1e4df97a24755c3b3
Brecht Nijman <79448658+brechtfm@users.noreply.github.com>
2024-10-16 11:30:36 +0200

    Update view_data

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -19,7 +19,7 @@ def main(args):
     for filename in in_files:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data), **(models.std_dev(inflammation_data))}
+        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data), 'std_dev': models.std_dev(inflammation_data)}
 
         views.visualize(view_data)
 
